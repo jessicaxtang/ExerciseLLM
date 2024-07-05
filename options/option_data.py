98d65831314586fdf742a3b486b546993e79be6d
jessicaxtang <jessicatang2019@gmail.com>
2024-07-05 18:20:31 -0400

    updated option_data.py args

--- options/option_data.py
+++ options/option_data.py
@@ -1,13 +1,16 @@
 import argparse
 
-def get_args():
+def get_args_parser():
     parser = argparse.ArgumentParser(description='Choose dataset preprocess type',
                                      add_help=True,
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
     # dataloader
     parser.add_argument('--dataname', type=str, default='UI-PRMD', help='dataset name') # UI-PRMD or IRDS
-    
+    parser.add_argument('--input_type', type=str, default='features', help='type of data preprocessing') # raw, features, or tokens
+    parser.add_argument('--downsample', type=int, default=5, help='downsampling rate') # 5 or 10
+    parser.add_argument('--joints', type=int, nargs='+', default=[1, 8, 10, 12, 14, 17, 21], help='list of most relevant joints') # from 1 to 22 for kinect, 1 to 39 for vicon
+
     ## UI-PRMD options
     parser.add_argument('--device', type=str, default='kinect', help='device used to capture data') # kinect or vicon
     parser.add_argument('--correctness', type=str, default='correct', help='binary exercise quality assessment') # correct or incorrect
