715bcc3c301b484274f884c092f71b607d0f05d7
jessicaxtang <jessicatang2019@gmail.com>
2024-07-26 19:12:39 -0400

    create LLM prompts automatically! with randint!!

--- create_prompt.py
+++ create_prompt.py
@@ -1,54 +1,87 @@
 import csv
 import os
+from random import randint
 
-def read_csv_and_write_to_txt(csv_file_path, txt_file_path, before_data, after_data):
-    # Read the CSV file
+def read_csv(csv_file_path):
     with open(csv_file_path, mode='r', newline='', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
         csv_content = [row for row in csv_reader]
-    
-    # Write to the text file
-    with open(txt_file_path, mode='w', encoding='utf-8') as txt_file:
-        txt_file.write('\n' + before_data)
-        for row in csv_content:
-            txt_file.write(','.join(row) + '\n')
-        txt_file.write('\n' + after_data)
+    return csv_content
+
+def write_to_txt(txt_file_path, text, newline_after=False):
+    with open(txt_file_path, mode='a', encoding='utf-8') as txt_file:
+        if newline_after:
+            txt_file.write(text + '\n')
+        else:
+            txt_file.write(text)
+            
+def generate_train_files(num_files, m, s, e):
+    '''
+    if m, s, or e is None, generate random values for them
+    returns a list of strings of file names in the format 'mXX_sXX_eXX'
+    note: specify the correctness of the files in the create_training_prompt() function
+    '''
+    train_files = []
+    for i in range(num_files):
+        if m == None:
+            m = randint(1, 11)
+        if s == None:
+            s = randint(1, 11)
+        if e == None:
+            e = randint(1, 11)
+        train_files.append(f'm{m:02d}_s{s:02d}_e{e:02d}')
+    return train_files
+
+def create_training_prompt(train_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, train_num):
+    '''
+    train_files: list of strings of file names in the format 'mXX_sXX_eXX'
+    exp_num: experiment number
+    train_num: training file number
+    '''
+    cor_tag = ''
+    if correctness == 'incorrect':
+        cor_tag = '_inc'
+
+    save_path_txt = save_dir_txt + f'/exp{exp_num}_train{train_num}{cor_tag}.txt'
+    ex_num = 1
+
+    for file in train_files:
+        csv_file_path = f'dataset/{dataname}_features/{correctness}/{device}/{input_type}/{file}{cor_tag}_{input_type}.csv'
+        data = read_csv(csv_file_path)
+        header = f'# {correctness} example {ex_num*train_num}'
+        write_to_txt(save_path_txt, header, newline_after=True)
+        for row in data:
+            write_to_txt(save_path_txt, ','.join(row)+ '\n')
+        ex_num += 1
 
 if __name__ == '__main__':
-    # define parameters
+    # define default parameters
     dataname = 'UI-PRMD'
     input_type = 'features'
     downsample_rate = 3
     device = 'kinect'
     correctness = 'correct'
     cor_tag = ''
-    subdir = 'positions'
+
+    # None for random value
     m = 7
     s = 1
-    e = 5
-
-    num_kp = 22 # turn into a parameter
-    num_axes = 3 # turn into a parameter
+    e = None
+    num_files = 10 # number of examples to include in each training prompt
+    num_train_files = 2 # number of training prompts to create per correctness
+    exp_num = 1
 
     count = 0
+
+    save_dir_txt = f'dataset/{dataname}_prompts/exp{exp_num}/{correctness}/{device}/{input_type}'
+    if not os.path.exists(save_dir_txt):
+        os.makedirs(save_dir_txt)
+
     for correctness in ['correct', 'incorrect']:
-        if correctness == 'incorrect':
-            cor_tag = '_inc'
-        for s in range(1, 11): # m
-            # Usage example
-            csv_file_path = 'dataset/{}_features/{}/{}/features/m{:02d}_s{:02d}_e{:02d}{}_features.csv'.format(dataname, correctness, device, m, s, e, cor_tag)
-            save_dir = 'dataset/{}_prompts/{}/{}/{}'.format(dataname, correctness, device, input_type)
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
-            
-            before_data = 'These are the angles of someone doing one rep of standing shoulder abduction exercise. Movement description: Subject raises one arm to the side by a lateral rotation, keeping the elbow and wrist straight.\n'
-            after_data = 'Tell me if the movement is done correctly within 50 words, in the format: [Correctness] [Most important feedback] [How I can improve these movements].'
-
-            save_path = '{}/m{:02d}_s{:02d}_e{:02d}{}_{}.txt'.format(save_dir, m, s, e, cor_tag, input_type)
-            print("Save path: ", save_path)
-
-            read_csv_and_write_to_txt(csv_file_path, save_path, before_data, after_data)
-
+        for train_num in range(1, 1+num_train_files):
+            train_files = generate_train_files(num_files, m, s, e)
+            print("Train files: ", train_files)
+            create_training_prompt(train_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, train_num)
             count += 1
 
     print("Total number of files saved: ", count)
