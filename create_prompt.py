07413206b901c0fe3d2e8c5f5ec3c15e732fc0bd
jessicaxtang <jessicatang2019@gmail.com>
2024-07-29 19:29:53 -0400

    generates test files too, with logger of files and labels used

--- create_prompt.py
+++ create_prompt.py
@@ -8,54 +8,74 @@ def read_csv(csv_file_path):
         csv_content = [row for row in csv_reader]
     return csv_content
 
-def write_to_txt(txt_file_path, text, newline_after=False):
+def append_to_txt(txt_file_path, text, newline_after=False):
     with open(txt_file_path, mode='a', encoding='utf-8') as txt_file:
         if newline_after:
             txt_file.write(text + '\n')
         else:
             txt_file.write(text)
             
-def generate_train_files(num_files, m, s, e):
+def generate_filenames(num_demos, m, s, e):
     '''
     if m, s, or e is None, generate random values for them
     returns a list of strings of file names in the format 'mXX_sXX_eXX'
-    note: specify the correctness of the files in the create_training_prompt() function
+    note: specify the correctness of the files in the create_prompt() function
     '''
-    train_files = []
-    for i in range(num_files):
+    filenames = []
+    for i in range(num_demos):
         if m == None:
-            m = randint(1, 11)
+            m = randint(1, 10)
         if s == None:
-            s = randint(1, 11)
+            s = randint(1, 10)
         if e == None:
-            e = randint(1, 11)
-        train_files.append(f'm{m:02d}_s{s:02d}_e{e:02d}')
-    return train_files
+            e = randint(1, 10)
+        filenames.append(f'm{m:02d}_s{s:02d}_e{e:02d}')
+    return filenames
 
-def create_training_prompt(train_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, train_num):
+def create_prompt(filenames, save_dir_txt, dataname, input_type, device, correctness, exp_num, file_type, file_num, num_files):
     '''
-    train_files: list of strings of file names in the format 'mXX_sXX_eXX'
+    filenames: list of strings of file names in the format 'mXX_sXX_eXX'
     exp_num: experiment number
-    train_num: training file number
+    file_type: 'demo' or 'test'
+    file_num: file number of file_type
+    num_files: total number of files to generate of file_type
     '''
     cor_tag = ''
     if correctness == 'incorrect':
         cor_tag = '_inc'
 
-    save_path_txt = save_dir_txt + f'/exp{exp_num}_train{train_num}{cor_tag}.txt'
-    ex_num = 1
+    save_path_txt = save_dir_txt + f'/exp{exp_num}_{file_type}{file_num}{cor_tag}.txt'
+    log_path_txt = save_dir_txt + f'/exp{exp_num}_{file_type}{file_num}{cor_tag}_log.txt'
+    ex_num = 1 + ((file_num-1)*num_files)
 
-    for file in train_files:
+    
+    for file in filenames:
+        # print("CURRENT FILE: ", file)
         csv_file_path = f'dataset/{dataname}_features/{correctness}/{device}/{input_type}/{file}{cor_tag}_{input_type}.csv'
+        if file_type == 'demo':
+            header = f'# {correctness} example {ex_num}'
+            append_to_txt(log_path_txt, correctness + '\t', newline_after=False)
+        else:
+            header = f'# unlabelled example {ex_num}'
+            correctness_rand = 'correct' if randint(0, 1) == 1 else 'incorrect' # generate random correctness per test example
+            print("correctness_rand: ", correctness_rand)
+            if correctness_rand == 'correct':
+                cor_tag = ''
+            else:
+                cor_tag = '_inc'
+            csv_file_path = f'dataset/{dataname}_features/{correctness_rand}/{device}/{input_type}/{file}{cor_tag}_{input_type}.csv'
+            append_to_txt(log_path_txt, correctness_rand + '\t', newline_after=False)
+
         data = read_csv(csv_file_path)
-        header = f'# {correctness} example {ex_num*train_num}'
-        write_to_txt(save_path_txt, header, newline_after=True)
+        append_to_txt(save_path_txt, header, newline_after=True) # write header to prompt file
         for row in data:
-            write_to_txt(save_path_txt, ','.join(row)+ '\n')
+            append_to_txt(save_path_txt, ','.join(row)+ '\n') # write data to prompt file
         ex_num += 1
+        append_to_txt(log_path_txt, file, newline_after=True) # write file name to log file
 
 if __name__ == '__main__':
-    # define default parameters
+    # define default parameters 
+    # TO DO: turn these into command line arguments
     dataname = 'UI-PRMD'
     input_type = 'features'
     downsample_rate = 3
@@ -67,21 +87,34 @@ if __name__ == '__main__':
     m = 7
     s = 1
     e = None
-    num_files = 10 # number of examples to include in each training prompt
-    num_train_files = 2 # number of training prompts to create per correctness
+    num_demos = 10 # number of examples to include in each demo prompt
+    num_demo_files = 2 # number of demo prompts to create per correctness
+    num_tests = 5 # number of examples to include in each test prompt
+    num_test_files = 2 # number of test prompts to create
     exp_num = 1
 
-    count = 0
+    demo_count, test_count = 0, 0
 
-    save_dir_txt = f'dataset/{dataname}_prompts/exp{exp_num}/{correctness}/{device}/{input_type}'
+    save_dir_txt = f'dataset/{dataname}_prompts/exp{exp_num}/{device}/{input_type}'
     if not os.path.exists(save_dir_txt):
         os.makedirs(save_dir_txt)
 
+    # generate demo prompts (labelled)
     for correctness in ['correct', 'incorrect']:
-        for train_num in range(1, 1+num_train_files):
-            train_files = generate_train_files(num_files, m, s, e)
-            print("Train files: ", train_files)
-            create_training_prompt(train_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, train_num)
-            count += 1
+        for demo_num in range(1, 1+num_demo_files):
+            demo_files = generate_filenames(num_demos, m, s, e)
+            print("demo files: ", demo_files)
+            create_prompt(demo_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, 'demo', demo_num, num_demos)
+            demo_count += 1
+    
+    for test_num in range(1, 1+num_test_files):
+            test_files = generate_filenames(num_tests, m, s, e)
+            # make sure test files are different from demo files
 
-    print("Total number of files saved: ", count)
+            print("test files: ", test_files)
+            create_prompt(test_files, save_dir_txt, dataname, input_type, device, correctness, exp_num, 'test', test_num, num_tests)
+            test_count += 1
+
+    print("num demo files saved:\t", demo_count)
+    print("num test files saved:\t", test_count)
+    print("TOTAL num files saved:\t", demo_count + test_count)
